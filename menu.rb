b6bc7d856df035d8b41c8889cd3fccbec05d35c1
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2020-12-11 10:03:44 +1000

    sticker price is now finished, working on menu

--- menu.rb
+++ menu.rb
@@ -1,7 +1,34 @@
 require ('tty-prompt')
+require_relative ('./sticker-price.rb')
 prompt = TTY::Prompt.new
-menu = [{name: "Financials", disabled: ""}, "Look at debt/equity", "Calculate sticker price","Print company report", "Exit"]
+menu = ["financials", "Look at debt/equity", "Calculate sticker price","Print company report", "Exit"]
+# Get ticker code and validate
+print "Enter ticker code: "
+ticker = gets.chomp.upcase
 
 while true
-    prompt.select("Select an option", menu, echo: false)
+    input = prompt.select("Select an option", menu)
+    case input
+    when "financials"
+        system "clear"
+        puts "hello"
+        puts "press ENTER key to continue"
+        gets
+    when "Look at debt/equity"
+        puts "under construction"
+        puts "press ENTER key to continue"
+        gets
+    when "Calculate sticker price"
+        system "clear"
+        sticker_price(ticker)
+        puts "press ENTER key to continue"
+        gets
+    when "Print company report"
+        puts "under construction"
+        puts "press ENTER key to continue"
+        gets
+    when "Exit"
+        break
+    end
 end
+puts "Exiting program..."
--- sticker-price.rb
+++ sticker-price.rb
@@ -1,21 +1,21 @@
 require ('httparty')
-# require_relative ('./stock-picker.rb')    
+require_relative ('./menu.rb')    
+def sticker_price(ticker)
 i = 1
 sticker_financials = ["eps_basic?period=FY", "total_equity?period=FY-9:FY", "price_to_earnings?period=FY-9:FY"]
 sticker_financials.each do |financial|
-    
-    query = "https://public-api.quickfs.net/v1/data/FMG:AU/#{financial}&api_key=e402e5e80284839d46c702e520e64add610df30d"
+    query = "https://public-api.quickfs.net/v1/data/#{ticker}:AU/#{financial}&api_key=e402e5e80284839d46c702e520e64add610df30d"
     response = HTTParty.get(query)
     # Parse JSON response and convert into array
     info = JSON.parse response.to_s
     arr = info["data"]
     case i
     when 1
-        @ttm_eps = arr
+        @ttm_eps = arr[0]
         puts @ttm_eps
     when 2
         @growth_rate = ((arr.last.to_f / arr.first.to_f) ** ( 1/10.0)) - 1
-        @growth_rate = (@growth_rate * 100).round(2)
+        @growth_rate = @growth_rate.round(2)
         puts @growth_rate
     when 3
         @pe_ratio = ((arr.sum) / 10).round(2)
@@ -24,19 +24,11 @@ sticker_financials.each do |financial|
     i += 1
 end
 
-
-# I need the following
-# EPS for the past financial year 2.24
-# total equity growth rate for the past however many years 23.88
-# average PE ratio for the past 10 years 9.28
-# "eps_basic?period=FY", "total_equity?period=FY-9:FY", "price_to_earnings?period=FY-9:FY",
-# ttm_eps = 2.24
-# growth_rate = 0.24
-# pe_ratio = 9.28
-
 future_eps = @ttm_eps * ((1 + @growth_rate) ** 10)
-puts future_eps
+puts "The future EPS will be #{future_eps.round(2)}"
 future_price = future_eps * @pe_ratio
-puts future_price
-puts "The sticker price is #{future_price / 4}"
-puts "The MOS price is #{future_price / 8}"
+puts "The future price will be #{future_price.round(2)}"
+puts "The sticker price is #{(future_price / 4).round(2)}"
+puts "The MOS price is #{(future_price / 8).round(2)}"
+end
+
